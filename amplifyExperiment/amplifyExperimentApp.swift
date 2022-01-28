1d308439fb040ee2d19f729295eb9ac180def179
evcodes <eddvar@amazon.com>
2022-01-28 18:19:10 -0500

    adding api to project

---
+++ amplifyExperiment/amplifyExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  amplifyExperimentApp.swift
+//  amplifyExperiment
+//
+//  Created by Varela, Eddy on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct amplifyExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
